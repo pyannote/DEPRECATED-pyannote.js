a680ff4ea1e61d1002385047dc157f4ef83b8f9d
Herve Bredin <bredin@limsi.fr>
2015-02-27 23:14:08 +0100

    feat: updating time marker

--- pyannote-annotation.js
+++ pyannote-annotation.js
@@ -19,20 +19,47 @@
     var sync = {'time': timeSync, 'data': dataSync};
     var plugin = pyannote.BasePlugin(name, sync, container);
 
-    plugin.draw = function() {
-      var h = '<h1>' + plugin.name + '</h1>';
-      var t = '<p>t = ' + plugin.get('time', 'currentTime') + '</p>';
-      var d = '<p>d = ' + plugin.get('data', 'whatever') + '</p>';
-      plugin.container.innerHTML = h + t + d;
-    }
+    plugin.config = {};
+    plugin.config.margin = 50;
+
+    plugin.svg = d3.select(plugin.container).append("svg");
+    plugin.svg.attr("height", 200)
+              .attr("class", "pyannote");
+
+    plugin.g = plugin.svg.append("g");
+    plugin.g.attr("transform", "translate(" + plugin.config.margin + ", 5)");
+    plugin.rect = plugin.g.append("rect")
+                          .attr("height", 50)
+                          .style("stroke", "gray")
+                          .style("stroke-width", 1)
+                          .style("fill", "white");
+
+    plugin.marker = plugin.g.append("rect")
+                            .attr("height", 50)
+                            .attr("width", 1)
+                            .style("stroke", "red")
+                            .style("stroke-width", 1);
+
+    plugin.scale = d3.scale.linear();
 
     plugin.update = function () {
-      plugin.draw();
+      var extent = plugin.get('time', 'extent');
+      plugin.scale.domain(extent);
+
+      var currentTime = plugin.get('time', 'currentTime');
+      plugin.marker.attr("transform", 
+                         "translate(" + plugin.scale(currentTime) + ", 0)");
     };
 
     plugin.resize = function () {
-      plugin.draw();
+      var width = plugin.container.clientWidth;
+      plugin.svg.attr("width", width);
+      plugin.scale.range([0, width-2*plugin.config.margin]);
+      plugin.rect.attr("width", width-2*plugin.config.margin);
+      plugin.update();
     }
+
+    plugin.resize();
 
     return plugin;
 
