8c5c2e518b48956a9742b6f25b7e8c05043fa8cd
Herve Bredin <bredin@limsi.fr>
2015-03-02 14:15:13 +0100

    chore: gather all svg in plugin.viz

--- pyannote-annotation.js
+++ pyannote-annotation.js
@@ -21,33 +21,40 @@
 
     plugin.config = {};
     plugin.config.margin = 50;
-
-    plugin.svg = d3.select(plugin.container).append("svg");
-    plugin.svg.attr("height", 200)
-              .attr("class", "pyannote");
-
-    plugin.g = plugin.svg.append("g");
-    plugin.g.attr("transform", "translate(" + plugin.config.margin + ", 5)");
-    plugin.rect = plugin.g.append("rect")
-                          .attr("height", 50)
-                          .style("stroke", "gray")
-                          .style("stroke-width", 1)
-                          .style("fill", "white");
-
-    plugin.marker = plugin.g.append("rect")
-                            .attr("height", 50)
-                            .attr("width", 1)
-                            .style("stroke", "red")
-                            .style("stroke-width", 1);
+    plugin.config.height = 50;
 
     plugin.scale = d3.scale.linear();
 
-    plugin.update = function () {
+    plugin.svg = d3.select(plugin.container).append("svg");
+    plugin.svg.attr("height", plugin.config.height)
+              .attr("class", "pyannote");
+
+    plugin.g = plugin.svg.append("g");
+    plugin.g.attr("transform", "translate(" + plugin.config.margin + ", 0)");
+    
+    plugin.viz = {};
+    plugin.viz.border = plugin.g.append("rect")
+                          .attr("height", plugin.config.height)
+                          .style("stroke", "#000")
+                          .style("shape-rendering", "crispEdges")
+                          .style("fill", "none");
+
+    plugin.viz.marker = plugin.g.append("line")
+                                .attr("x1", 0)
+                                .attr("y1", 0)
+                                .attr("x2", 0)
+                                .attr("y2", plugin.config.height)
+                                .attr("stroke-width", 3)
+                                .style("shape-rendering", "crispEdges")
+                                .style("stroke", "red");
+
+    plugin.update = function (category, property, broadcaster, old_value, new_value) {
+
       var extent = plugin.get('time', 'extent');
       plugin.scale.domain(extent);
 
       var currentTime = plugin.get('time', 'currentTime');
-      plugin.marker.attr("transform", 
+      plugin.viz.marker.attr("transform", 
                          "translate(" + plugin.scale(currentTime) + ", 0)");
     };
 
@@ -55,7 +62,8 @@
       var width = plugin.container.clientWidth;
       plugin.svg.attr("width", width);
       plugin.scale.range([0, width-2*plugin.config.margin]);
-      plugin.rect.attr("width", width-2*plugin.config.margin);
+
+      plugin.viz.border.attr("width", width-2*plugin.config.margin);
       plugin.update();
     };
 
