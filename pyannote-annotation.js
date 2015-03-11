1b2da05942a7d0058449dd5167c98091998a8a87
Herve Bredin <bredin@limsi.fr>
2015-03-11 11:13:20 +0100

    feat: annotation chart initial import

--- pyannote-annotation.js
+++ pyannote-annotation.js
@@ -132,6 +132,81 @@
 
   };
 
+  my.Chart = function (name, container, height, dataSync) {
+
+    var sync = {'data': dataSync};
+    var plugin = pyannote.Plugin(name, sync, container, height);
+
+    plugin._update = function(content, colorScale) {
+
+      var duration = {};
+      for (var i = content.length - 1; i >= 0; i--) {
+        var label = content[i].label;
+        if (!(label in duration)) { duration[label] = 0.; }
+        duration[label] += content[i].segment.end - content[i].segment.start;
+      };
+
+      var radius = Math.min(plugin.config.width, 
+                            plugin.config.height) / 2;
+
+      var arc = d3.svg.arc()
+                      .outerRadius(radius - 10)
+                      .innerRadius(radius - 80);
+
+      var pie = d3.layout.pie()
+                         .sort(null)
+                         .value(function(d) { return d.value; });
+
+      plugin.pie = plugin.content.append("g")
+                                 .attr("transform", "translate(" + plugin.config.width/2 + ", " + plugin.config.height/2 + ")");
+
+      var g = plugin.pie.selectAll(".arc")
+                    .data(pie(d3.entries(duration)))
+                    .enter().append("g")
+
+                            .attr("class", "arc");
+
+      g.append("path")
+       .attr("d", arc)
+       .style("fill", function(d) { return colorScale(d.data.key); });
+
+      g.append("text")
+          .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+          .attr("dy", ".35em")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d.data.key; });
+
+    };
+
+
+    plugin.update = function (category, property, broadcaster, old_value, new_value) {
+
+
+      if (category === 'data' && property === 'content') {
+        var color = d3.scale.category20();
+        plugin.set('data', 'color', color);
+        plugin._update(new_value, color); 
+        return;
+      }
+
+      if (broadcaster !== plugin.name && category === 'data' && property === 'color') {
+        var content = plugin.get('data', 'content');
+        if (content === undefined) { content = []; }
+        plugin._update(content, new_value);
+      }
+
+    };
+
+    plugin.resize = function () {
+    
+    };
+
+    plugin.resize();
+
+    return plugin;
+
+  };
+
   pyannote.annotation = my;
 
   return my;
